f22eedd9a41bd09f8b400fd6492c1b3b36cf08cf
halhosono <50980491+halhosono@users.noreply.github.com>
2019-05-30 16:30:51 +0900

    Create setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -OutFile ConfigureRemotingForAnsible.ps1
+powershell -ExecutionPolicy RemoteSigned .\ConfigureRemotingForAnsible.ps1
